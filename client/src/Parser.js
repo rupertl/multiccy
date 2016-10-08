b81ee7cb7d33532568328da18731df98432db297
Rupert Lane <rupert@rupert-lane.org>
2016-10-08 20:04:34 +0900

    Improved Parser
    * Allow 100 USD or USD 100
    * Allow 100USD
    * Ensure the right number of currencies and amounts are provided

--- client/src/Parser.js
+++ client/src/Parser.js
@@ -1,24 +1,37 @@
 // Class to take a request string and extract amount and from/to currencies
 
-const stopWords = new Set(['TO', 'INTO']);
+const STOP_WORDS = new Set(['TO', 'INTO']);
+const NUMBER_RE = /-*[0-9.]+/;
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 export default function parse(text) {
     if (typeof text !== 'string') {
         return {error: "Please provide something to convert"};
     }
 
-    const words = text
+    const tokens = text
+          // Make "100USD" or "USD100" separate tokens
+          .replace(NUMBER_RE, num => " " + num + " ")
           .trim()
           .split(/\s+/)
-          .map(word => word.toUpperCase())
-          .filter(word => !stopWords.has(word));
+          .map(token => token.toUpperCase())
+          .filter(token => ! STOP_WORDS.has(token));
 
-    if (words.length < 3) {
+    if (tokens.length < 3) {
         return {error: "Please provide an amount and two currencies"};
     }
-    const fromAmount = Number(words[0]);
-    const fromCcy = words[1];
-    const toCcy = words[2];
+
+    const numericTokens = tokens.map(isNumeric);
+    if (numericTokens.filter(token => token).length !== 1) {
+        return {error: "Please provide one amount and two currencies"};
+    }
+
+    const fromAmount = Number(numericTokens[0] ? tokens[0] : tokens[1]);
+    const fromCcy = numericTokens[0] ? tokens[1] : tokens[0];
+    const toCcy = tokens[2];
 
     return {fromAmount, fromCcy, toCcy};
 }
--- client/src/Parser.test.js
+++ client/src/Parser.test.js
@@ -26,6 +26,22 @@ it('extracts from ccy if extra whitespace', () => {
     expect(Parser("  100   JPY  to  GBP   ").fromCcy).toBe("JPY");
 });
 
+it('extracts from amount if no whitespace after', () => {
+    expect(Parser("100JPY to GBP").fromAmount).toBe(100);
+});
+
+it('extracts complex from amount if no whitespace after', () => {
+    expect(Parser("-100.23GBP to JPY").fromAmount).toBe(-100.23);
+});
+
+it('extracts from ccy if no whitespace before', () => {
+    expect(Parser("100JPY to GBP").fromCcy).toBe("JPY");
+});
+
+it('extracts from amount if before from ccy', () => {
+    expect(Parser("JPY100 to GBP").fromAmount).toBe(100);
+});
+
 it('errors if nothing provided', () => {
     expect(Parser().error).toBeDefined();
 });
@@ -44,4 +60,24 @@ it('errors if no from ccy provided', () => {
 
 it('errors if no to ccy provided', () => {
     expect(Parser("100 gbp to").error).toBeDefined();
+});
+
+it('errors if no from amount provided', () => {
+    expect(Parser("gbp usd").error).toBeDefined();
+});
+
+it('errors if no from amount provided but three words', () => {
+    expect(Parser("gbp jpy usd").error).toBeDefined();
+});
+
+it('errors if a bad number is provided', () => {
+    expect(Parser("100.23.45 gbp to usd").error).toBeDefined();
+});
+
+it('errors if two numbers are provided', () => {
+    expect(Parser("100 200 gbp to usd").error).toBeDefined();
+});
+
+it('errors if a number is provided instead of a to ccy', () => {
+    expect(Parser("100 gbp to 200").error).toBeDefined();
 });
